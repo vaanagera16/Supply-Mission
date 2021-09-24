dffe93c76952d3c1e59d48b2f41f680427f391b4
vaanagera16 <88883715+vaanagera16@users.noreply.github.com>
2021-09-24 15:19:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,102 +1,79 @@
-//name spacing
+var helicopterImg, bgImg;
+var helicopterSprite, packageSprite;
+var packageBody,boxBottomBody, boxLeftBody, boxRightBody;
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 
-// Only for debugging code, is this needed, not otherwise
-// const Render = Matter.Render;
 
-var myengine,myworld;
-var ground, ball;
-
-function setup(){
-    var canvas = createCanvas(800,400);
-   
-    myengine = Engine.create();
-    myworld = myengine.world;
-
-    //Code for brown ground
-    var ground_options ={
-        isStatic: true,
-      
-    }
-    ground = Bodies.rectangle(400,390,800,20,ground_options);
-    World.add(myworld,ground);
-    console.log(ground);
-
-    // Code for red box
-    var redBox_options ={
-        isStatic: true,
-    }
-
-    redBox = Bodies.rectangle(250,320,100,120,redBox_options);
-    World.add(myworld,redBox);
-
-    // Code for green box
-    var greenBox_options ={
-        isStatic: true,
-    }
-
-    greenBox = Bodies.rectangle(450,320,100,120,greenBox_options);
-    World.add(myworld,greenBox);
-
-    // Code for yellow box
-    var yellowBox_options ={
-        isStatic: true,
-        angle:-60
-        
-    }
-
-    yellowBox = Bodies.rectangle(590,320,100,20,yellowBox_options);
-    // Matter.Body.setAngle(yellowBox,130)
-    World.add(myworld,yellowBox);
-
-    // Code for ball
-    var ball_options ={
-        isStatic: false,
-        restitution: 1.28   }
-
-    ball = Bodies.circle(620,100,20, ball_options);
-    World.add(myworld,ball);
-    
-
-    // Only for debugging code, is this needed, not otherwise
-    // var render = Render.create({
-    //   element: document.body,
-    //   engine: myengine,
-    //   options: {
-    //     width: 1600,
-    //     height: 700,
-    //     wireframes: false
-    //   }
-    // });
-    // Render.run(render);
-
-    //console.log(ball);
+function preload()
+{
+	helicopterImg=loadImage("helicopter.png")
+	bgImg=loadImage("bg.png")
 }
 
-function draw(){
-    background("lightgreen");
-    Engine.update(myengine);
-    rectMode(CENTER);
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	engine = Engine.create();
+	world = engine.world;
 
-    fill("brown")
-    rect(ground.position.x,ground.position.y,800,20);
-    fill("red")
-    rect(redBox.position.x,redBox.position.y,100,120);
-    fill("green")
-    rect(greenBox.position.x,greenBox.position.y,100,120);
-    
-    fill("yellow")
-    push();
-    translate(yellowBox.position.x,yellowBox.position.y);
-    rotate(-45);
-    rectMode(CENTER);
-    rect(0, 0, 100, 20);
-    pop();
-   
-    fill("red")
-    ellipseMode(RADIUS);
-    ellipse(ball.position.x, ball.position.y, 20, 20);
+	packageSprite=createSprite(width/2, 50,200,200);
+	packageSprite.shapeColor = "yellow"
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 100, 10,10);
+	helicopterSprite.addImage(helicopterImg)
+	helicopterSprite.scale=0.6
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	boxBottomBody = new Box(400, 610, 200,20);
+ 	boxLeftBody = new Box(310, 570, 20,100);
+ 	boxRightBody = new Box(490, 570, 20,100);
+
+	packageBody = Bodies.circle(width/2 , 100 , 20 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+
 }
+
+
+function draw() {
+	Engine.update(engine);
+	background(bgImg);
+	
+	packageSprite.x= packageBody.position.x 
+	packageSprite.y= packageBody.position.y 
+
+	boxRightBody.display();
+	boxLeftBody.display();
+	boxBottomBody.display();
+
+	drawSprites(); 
+}
+
+function keyPressed() {
+	
+	if (keyCode === LEFT_ARROW) {
+
+		helicopterSprite.x=helicopterSprite.x-20;    
+		translation={x:-20,y:0}
+		Matter.Body.translate(packageBody, translation)
+
+	  } else if (keyCode === RIGHT_ARROW) {
+
+		helicopterSprite.x=helicopterSprite.x+20;
+		translation={x:20,y:0}
+		Matter.Body.translate(packageBody, translation)
+		
+	  }
+	  
+	if (keyCode === DOWN_ARROW) {
+	   //make the static property of packageBody as false
+	   Matter.Body.setStatic(packageBody, false);
+
+	}
+}
+  
